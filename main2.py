47ffc7eedc943aa1438e12c63f0fa430e9a11a6a
123anEl <onlan.anel2003@gmail.com>
2023-09-24 16:07:46 +0600

    Initial commit. Initialization off block properties Function for block addition

--- main2.py
+++ main2.py
@@ -1,5 +1,4 @@
 import datetime
-from random import randint
 import hashlib
 
 '''def f1():
@@ -11,52 +10,47 @@ import hashlib
             return unique_nonce'''
 
 class BlockChain:
-    index = 0
+    difficulty = 3
+    index = -1
     blocks = []
-    def __init__(self, data=str):
+    def __init__(self, sender, recipient, amount):
         BlockChain.index += 1
         self.index = BlockChain.index
-        prev_index= self.index-1
-        if self.index>1:
-            previous_hash = BlockChain.blocks[prev_index-1].hash
+        prev_index = self.index-1
+        sender = sender
+        recipient = recipient
+        amount = amount
+        if self.index>=1:
+            previous_hash = BlockChain.blocks[prev_index].hash
+            self.transaction_data = str(sender)+str(recipient)+str(amount)
            # previous_hash='0000'
         else:
-            previous_hash = '0000'
+            previous_hash = '0' # If index is 0, then it is genesis block. Genesis block's previous hash is 0. Because it has no previous block to reference to
+            self.transaction_data = [] #For genesis block there is no transaction yet
         self.timeStamp = datetime.datetime.now()
-        self.data = data
-        self.nonce = randint(0, 2**32)
-        self.block_data = (str(previous_hash) + '+' + str(data)).encode()
+        self.nonce = 0
+        self.block_data = (str(previous_hash) + '+' + str(self.__dict__)).encode()
         self.hash = hashlib.sha256(self.block_data).hexdigest()
         BlockChain.blocks.append(self)
 
-tr1 = 'b1 sends MMM 2.5 btc'
-b1 = BlockChain(tr1)
-print(b1.index)
-print(b1.timeStamp)
-print(b1.data)
-print(b1.nonce)
-print(b1.block_data)
-print(b1.hash)
-print()
+def get_info_to_add_block():
+    sender = input('Enter sender:')
+    recipient = input('Enter recipient:')
+    amount = input('Enter amount:')
+    block = BlockChain(sender, recipient, amount)
+    answer = input('Want to add more blocks? (yes/no)')
+    if answer=='yes':
+        return get_info_to_add_block()
+    else:
+        print('Successful addition of blocks')
 
-tr2 = 'b2 sends YYY 2.5 btc'
-b2 = BlockChain(tr2)
-print(b2.index)
-print(b2.timeStamp)
-print(b2.data)
-print(b2.nonce)
-print(b2.block_data)
-print(b2.hash)
 
-print()
-print(BlockChain.blocks[0].index)
 
-tr3 = 'b3 sends WWW 3.5 btc'
+get_info_to_add_block()
+
+for block in BlockChain.blocks:
+    print(block.__dict__)
+'''tr3 = 'b3 sends WWW 3.5 btc'
 b3 = BlockChain(tr3)
-print(b3.index)
-print(b3.timeStamp)
-print(b3.data)
-print(b3.nonce)
-print(b3.block_data)
-print(b3.hash)
-
+print(b3.__dict__) # Print all attributes of an object
+'''
