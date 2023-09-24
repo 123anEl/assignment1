665b1e5bd1db51b8aec51bfbb957c817f9ed190a
123anEl <onlan.anel2003@gmail.com>
2023-09-24 19:14:03 +0600

    Initial commit. Initialization off block properties Function for block addition Validating and calculation of nonce

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 import datetime
 import hashlib
+import json
 
 '''def f1():
     list_of_nonce=[]
@@ -10,10 +11,11 @@ import hashlib
             return unique_nonce'''
 
 class BlockChain:
-    difficulty = 3
+    difficulty = 2
     index = -1
     blocks = []
-    def __init__(self, sender, recipient, amount):
+    transactions=[]
+    def __init__(self, sender, recipient, amount, nonce):
         BlockChain.index += 1
         self.index = BlockChain.index
         prev_index = self.index-1
@@ -21,35 +23,57 @@ class BlockChain:
         recipient = recipient
         amount = amount
         if self.index>=1:
-            previous_hash = BlockChain.blocks[prev_index].hash
+            self.previous_hash = BlockChain.blocks[prev_index].hash
             self.transaction_data = str(sender)+str(recipient)+str(amount)
            # previous_hash='0000'
         else:
-            previous_hash = '0' # If index is 0, then it is genesis block. Genesis block's previous hash is 0. Because it has no previous block to reference to
+            self.previous_hash = '0' # If index is 0, then it is genesis block. Genesis block's previous hash is 0. Because it has no previous block to reference to
             self.transaction_data = [] #For genesis block there is no transaction yet
         self.timeStamp = datetime.datetime.now()
-        self.nonce = 0
-        self.block_data = (str(previous_hash) + '+' + str(self.__dict__)).encode()
+        self.nonce = nonce
+        self.block_data = json.dumps(self.__dict__, sort_keys=True, default=str).encode()
         self.hash = hashlib.sha256(self.block_data).hexdigest()
-        BlockChain.blocks.append(self)
+        BlockChain.transactions.append(self.transaction_data) #Add transaction to the list
+        BlockChain.blocks.append(self)#Add block to the list of blocks
 
-def get_info_to_add_block():
+genesis_block = BlockChain('','', 0, 0)
+
+def validate(transactions, last_block_hash, nonce ):
+    answer = (str(transactions)+str(last_block_hash)+str(nonce)).encode()
+    answer_hash= hashlib.sha256(answer).hexdigest()
+    #print(answer_hash)
+    return answer_hash[0:2]=='00'
+
+def calculate_nonce():
+    nonce = 0
+    last_block = BlockChain.blocks[-1]
+    last_block_hash = BlockChain.blocks[-1].hash
+    while not validate(BlockChain.transactions, last_block_hash, nonce):
+        nonce+=1
+    return nonce
+
+
+#calculate_nonce()
+#print(calculate_nonce())
+def get_transactionInfo_to_add_block():
     sender = input('Enter sender:')
     recipient = input('Enter recipient:')
     amount = input('Enter amount:')
-    block = BlockChain(sender, recipient, amount)
+    nonce = calculate_nonce()
+    block = BlockChain(sender, recipient, amount, nonce)
     answer = input('Want to add more blocks? (yes/no)')
     if answer=='yes':
-        return get_info_to_add_block()
+        return get_transactionInfo_to_add_block()
     else:
         print('Successful addition of blocks')
 
 
 
-get_info_to_add_block()
+get_transactionInfo_to_add_block()
 
 for block in BlockChain.blocks:
     print(block.__dict__)
+
 '''tr3 = 'b3 sends WWW 3.5 btc'
 b3 = BlockChain(tr3)
 print(b3.__dict__) # Print all attributes of an object
